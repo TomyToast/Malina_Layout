bfc31321062c322e5bd95a8216d29db9e90a6f1e
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-01-12 17:56:43 +0100

    almost finished hamburger menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,27 @@
 require("../sass/main/style.scss");
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    const hamburger  = document.getElementById('hamburger_menu');
+    const list = document.getElementsByClassName('header_menu_list');
+    const listNode = list[0].style.display = 'none';
+    let bool = true;
+
+    hamburger.addEventListener("click", () => {
+            list[0].style.display = "block";
+    });
+
+    //   var doToggle = function(e) {
+    //     e.preventDefault();
+    //     menuToggle.classList.toggle('header_menu_list');
+    //     list.classList.toggle('header_menu_list');
+    //     console.log('działam');
+    //   }
+
+    // menuToggle.addEventListener('click', function(e) { doToggle(e); });
+    // list.addEventListener('click', function(e) { doToggle(e); });
+  });
+
 
 
